156ddfd77ca07a0a067a3d0a9d34a785205eebb6
Asim <78923699+AsimAlam@users.noreply.github.com>
2026-03-01 00:29:45 +0530

    handling frontend cors in backend

--- backend/realtimeNotify/src/main/java/org/example/realtimenotify/config/WebSocketConfig.java
+++ backend/realtimeNotify/src/main/java/org/example/realtimenotify/config/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry
         .addEndpoint("/ws")
-        .setAllowedOriginPatterns("*")
+        .setAllowedOriginPatterns("https://realtime-notifications-service-frontend.onrender.com")
         .setHandshakeHandler(new UserHandshakeHandler())
         .addInterceptors(new JwtHandshakeInterceptor())
         .withSockJS();
